bbaf7e37e748f355fb7d5c4587713a4b9ab1d413
WALLOSCHKE Michael <michael.walloschke@axians.de>
2024-07-11 18:32:06 +0200

    feat(project): Add documentation, linting, and testing setup - Add documentation configuration for project name, source URL, and optional homepage URL - Add Credo, Dialyxir, and Ex_doc dependencies for linting and documentation generation - Add aliases for running tests, dialyzer, Credo, and coverage checks - Add module documentation for UserMgmtTest module - Add .DS_Store and .elixir_ls to .gitignore

--- apps/user_mgmt/test/user_mgmt_test.exs
+++ apps/user_mgmt/test/user_mgmt_test.exs
@@ -1,4 +1,7 @@
 defmodule UserMgmtTest do
+  @moduledoc """
+  This is a test module for UserMgmt.
+  """
   use ExUnit.Case
   doctest UserMgmt
 
--- mix.exs
+++ mix.exs
@@ -6,7 +6,19 @@ defmodule ExDca.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      dialyzer: [plt_add_deps: :app_tree],
+      deps: deps(),
+      aliases: aliases(),
+
+      # Docs
+      name: "ExDca",
+      source_url: "https://github.com/Taraluktus/ex_dca"
+      #   homepage_url: "http://YOUR_PROJECT_HOMEPAGE",
+      # docs: [
+      #   main: "MyApp", # The main page in the docs
+      #   logo: "path/to/logo.png",
+      #   extras: ["README.md"]
+      # ]
     ]
   end
 
@@ -16,6 +28,27 @@ defmodule ExDca.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:credo, "~> 1.7", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  #
+  # Aliases listed here are available only for this project
+  # and cannot be accessed from applications inside the apps/ folder.
+  defp aliases do
+    [
+      # run `mix setup` in all child apps
+      setup: ["cmd mix setup"],
+      ensure_consistency: ["test", "dialyzer", "credo --strict", "coveralls"]
+    ]
   end
 end
